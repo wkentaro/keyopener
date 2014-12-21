64f16577d85edcc771536ca0f86833ca1896f485
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-21 15:57:41 +0900

    change datetime time access with more beautiful way

--- code/speak_time_signal.py
+++ code/speak_time_signal.py
@@ -13,8 +13,8 @@ from speak_raspi import SpeakRaspi
 def speak_time_signal():
     # get time
     now = datetime.datetime.now()
-    hour = num2words.num2words(int(now.strftime('%H')))
-    minute = num2words.num2words(int(now.strftime('%M')))
+    hour = num2words.num2words(now.hour)
+    minute = num2words.num2words(now.minute)
     # compose the sentence
     sentence = 'The time is {hour} {minute}.'.format(hour=hour, minute=minute)
     filename = 'time_signal.mp3'
