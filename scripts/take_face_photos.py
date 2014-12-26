28835f8fa516df8f43549d23dd8f838b46dac155
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-27 02:05:09 +0900

    update take_face_photos.py make detect face to classmethod

--- scripts/take_face_photos.py
+++ scripts/take_face_photos.py
@@ -13,23 +13,31 @@ from take_photos import TakePhotos
 
 
 class TakeFacePhotos(TakePhotos):
-    def __init__(self, limit):
+    def __init__(self):
         super(TakeFacePhotos, self).__init__()
-        self.limit = limit
         self.face_exists = False
 
     def _event_handle(self):
-        vis = self._detect_face()
+        faces = self.detect_face(self.frame)
+        if len(faces) > 1:
+            self.face_exists = True
+        vis = self._draw_face_rect(faces)
         if self.face_exists is True:
             self._save_frame()
-        if len(os.listdir('../data/raw/')) >= self.limit:
-            self.end_flag = True
         cv2.imshow('detect_face', vis)
         k = cv2.waitKey(100)
         if k == 27:
             self.end_flag = True
 
-    def _detect_face(self):
+    def _draw_face_rect(self, faces):
+        # draw rectangle around the face
+        vis = self.frame.copy()
+        for (x, y, w, h) in faces:
+            cv2.rectangle(vis, (x,y), (x+w,y+h), (0,255,0), 1)
+        return vis
+
+    @classmethod
+    def detect_face(self, img):
         # load cascade
         cascade_files = [
             '/usr/local/Cellar/opencv/2.4.9/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml',
@@ -39,26 +47,20 @@ class TakeFacePhotos(TakePhotos):
             ]
         filename = cascade_files[0]
         face_cascade = cv2.CascadeClassifier(filename=filename)
-        frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # detect face
         faces = face_cascade.detectMultiScale(
-                frame_gray,
+                imgray,
                 scaleFactor=1.1,
                 minNeighbors=5,
                 minSize=(30, 30),
                 flags=cv2.cv.CV_HAAR_SCALE_IMAGE,
             )
-        if len(faces) > 1:
-            self.face_exists = True
-        # draw rectangle around the face
-        vis = self.frame.copy()
-        for (x, y, w, h) in faces:
-            cv2.rectangle(vis, (x,y), (x+w,y+h), (0,255,0), 1)
-        return vis
+        return faces
 
 
 def main():
-    t = TakeFacePhotos(limit=100)
+    t = TakeFacePhotos()
     t.capture_loop()
 
 
