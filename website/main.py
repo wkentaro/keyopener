16f29b9368969e81d4e7cdb259e8096a37db0398
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-17 15:24:10 +0900

    webapp2 -> flask

--- website/main.py
+++ website/main.py
@@ -1,34 +1,9 @@
-#!/usr/bin/env python
-#
-import os
+from flask import Flask
+app = Flask(__name__, static_url_path='/static')
 
-import webapp2
-import jinja2
+@app.route('/')
+def hello_world():
+    return "Hello World!"
 
-
-view_path = os.path.dirname(__file__)
-JINJA_ENVIRONMENT = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(view_path),
-        extensions=['jinja2.ext.autoescape'],
-        autoescape=True)
-
-
-class Base(webapp2.RequestHandler):
-    def render(self, html, values={}):
-        template = JINJA_ENVIRONMENT.get_template(html)
-        self.response.write(template.render(values))
-
-
-class Index(Base):
-    def get(self):
-        # output = 'webapp2 running on apache2'
-        # self.response.headers = [
-        #         ('Content-type', 'text/plain'),
-        #         ('Content-length', str(len(output)))]
-        # self.response.out.write(output)
-        self.render(html='index.html')
-
-
-application = webapp2.WSGIApplication(
-        [('/', Index)],
-        debug=True)
+if __name__ == '__main__':
+    app.run()
